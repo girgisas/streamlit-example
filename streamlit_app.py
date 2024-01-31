d2e956cbebdd92696dd703a14e5285a5370125bd
Ashley Girgis <114462489+girgisas@users.noreply.github.com>
2024-01-31 03:05:03 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,34 +7,8 @@ from PIL import Image
 import base64
 import os
 
-# Paths to images
-thumb_paths = ["thumb1.jpg", "thumb2.jpg", "thumb3.jpg", "thumb4.jpg"]
-info_paths = ["1.jpg", "2.jpg", "3.jpg", "4.jpg"]
-
-import streamlit as st
-from st_clickable_images import clickable_images
-
-clicked = clickable_images(
-    [
-        "thumb1.jpg",
-        "thumb1.jpg",
-        "thumb1.jpg",
-        "thumb1.jpg",
-    ],
-    titles=[f"Image #{str(i)}" for i in range(4)],
-    div_style={"display": "flex", "justify-content": "center", "flex-wrap": "wrap"},
-    img_style={"margin": "5px", "height": "200px"},
-)
-
-st.markdown(f"Image #{clicked} clicked" if clicked > -1 else "No image clicked")
-
 # Streamlit layout
 st.title("Ashley Girgis | Coding Portfolio")
-
-import streamlit as st
-from PIL import Image
-import base64
-import os
 
 # Load your images
 thumb1 = Image.open("thumb1.jpg")
@@ -62,9 +36,6 @@ def get_image_as_base64(path):
 # Paths to images
 thumb_paths = ["thumb1.jpg", "thumb2.jpg", "thumb3.jpg", "thumb4.jpg"]
 info_paths = ["1.jpg", "2.jpg", "3.jpg", "4.jpg"]
-
-# Streamlit layout
-st.title("Interactive Infographic Viewer")
 
 # Display thumbnails in columns
 # Display thumbnails as hyperlinks
