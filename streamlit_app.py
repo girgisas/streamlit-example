c9b58faad1c05eae2653b95d9f52f973ad6065a3
Ashley Girgis <114462489+girgisas@users.noreply.github.com>
2024-01-31 01:04:30 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,6 +11,23 @@ import os
 thumb_paths = ["thumb1.jpg", "thumb2.jpg", "thumb3.jpg", "thumb4.jpg"]
 info_paths = ["1.jpg", "2.jpg", "3.jpg", "4.jpg"]
 
+import streamlit as st
+from st_clickable_images import clickable_images
+
+clicked = clickable_images(
+    [
+        "thumb1.jpg",
+        "thumb1.jpg",
+        "thumb1.jpg",
+        "thumb1.jpg",
+    ],
+    titles=[f"Image #{str(i)}" for i in range(4)],
+    div_style={"display": "flex", "justify-content": "center", "flex-wrap": "wrap"},
+    img_style={"margin": "5px", "height": "200px"},
+)
+
+st.markdown(f"Image #{clicked} clicked" if clicked > -1 else "No image clicked")
+
 # Streamlit layout
 st.title("Ashley Girgis | Coding Portfolio")
 
